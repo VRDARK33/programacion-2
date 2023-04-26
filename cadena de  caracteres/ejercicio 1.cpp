8717bceef53990c9606ddcfb702529bb33158bb7
VRDARK33 <brayan54sd@gmail.com>
2023-04-25 21:40:00 -0500

    se cambio algo en el programa 1

--- cadena de  caracteres/ejercicio 1.cpp
+++ cadena de  caracteres/ejercicio 1.cpp
@@ -2,17 +2,17 @@
 ver de c++ 5.11 realizados por brayan andres sanchez perez
 05/09/2020
 
-1. Escribir un programa que lea una frase caracteres a carácter utilizando scanf. */
+1. Escribir un programa que lea una frase caracteres a carï¿½cter utilizando scanf. */
 
 #include<iostream>
 #include<stdio.h>
 using namespace std;
 
 int main(){
-	int x,t;
+	int x;
 	char b[100],y;
 	
-	printf("ingrese el tamaño de la frase: \n");scanf("%d",&x);
+	printf("ingrese el tamaï¿½o de la frase: \n");scanf("%d",&x);
 	
 	for(int i=0;i<=x;i++){	
 		scanf("%c",&y);
@@ -20,7 +20,7 @@ int main(){
 		fflush(stdin);
 	}
 	
-	cout<<b;
+	cout<<b<<endl;
 	
 	return 0; 
 }
